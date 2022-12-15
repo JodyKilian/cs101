c3b1385fab49302a1dd088687beb88464b314114
B1029045 <90835424+JodyKilian@users.noreply.github.com>
2022-12-15 16:59:28 +0800

    Create main0.c

---
+++ hw6-111/main0.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int i, n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    i = n;
+  
+    if(i > 0) {
+        while(i % 2 == 0) {
+            i = i / 2;
+        }
+        if(i == 1) {
+            printf("%d is a power of 2", n);
+        } else {
+            printf("%d is not a power of 2", n);
+        }
+    }
+    return 0;
+}
