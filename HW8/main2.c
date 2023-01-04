0ac7a61aaf380cda01e3f654e34522871aacf1e6
B1029045 <90835424+JodyKilian@users.noreply.github.com>
2023-01-04 16:27:06 +0800

    Create main2.c

---
+++ HW8/main2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int i = 15;
+    int count = 0;
+    while(i > 0) {
+        if(i % 2 == 1) {
+            count++;
+        }
+        i /= 2;
+    }
+    printf("%d\n", count);
+    return 0;
+}
